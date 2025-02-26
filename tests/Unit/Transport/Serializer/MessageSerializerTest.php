5c7f2770935ce7a0b3fcbe9393e66e9bfa57dbf4
AlvaroRosado <alvaro.rosado93@gmail.com>
2025-02-26 10:18:47 +0100

    added unit test battery

--- tests/Unit/Transport/Serializer/MessageSerializerTest.php
+++ tests/Unit/Transport/Serializer/MessageSerializerTest.php
@@ -211,16 +211,3 @@ class ClassWithEmptyTypeMethod
         return '';
     }
 }
-
-class TestSymfonyStamp
-{
-    public function __construct(
-        private string $value
-    ) {
-    }
-
-    public function getValue(): string
-    {
-        return $this->value;
-    }
-}
